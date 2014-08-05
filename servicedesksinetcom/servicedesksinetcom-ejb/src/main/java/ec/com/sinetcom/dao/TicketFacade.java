af31aaffe92f7a01241f0ac0e2f49065d1bb0510
diegoaflores88 <diegoaflores88@a04695b4-e77e-5f7e-00ee-a7b27f77682c>
2014-08-05 20:44:12 +0000

    JPA Facade Ticket

--- servicedesksinetcom/servicedesksinetcom-ejb/src/main/java/ec/com/sinetcom/dao/TicketFacade.java
+++ servicedesksinetcom/servicedesksinetcom-ejb/src/main/java/ec/com/sinetcom/dao/TicketFacade.java
@@ -4,6 +4,9 @@
  */
 package ec.com.sinetcom.dao;
 
+import ec.com.sinetcom.orm.EstadoTicket;
+import ec.com.sinetcom.orm.PrioridadTicket;
+import ec.com.sinetcom.orm.ServicioTicket;
 import ec.com.sinetcom.orm.Ticket;
 import ec.com.sinetcom.orm.Usuario;
 import java.util.List;
@@ -31,13 +34,80 @@ public class TicketFacade extends AbstractFacade<Ticket> {
     }
     
     /**
-     * Función que permite recuperar los tickets abiertos de un determinado usuario propietario
+     * Función que permite recuperar los tickets por estado de un determinado usuario propietario
      */
-    public List<Ticket> obtenerTicketsAbiertosDePropietario(Usuario usuario){
-        String sql = "SELECT t FROM Ticket t JOIN t.estadoTicketcodigo e WHERE t.usuarioidpropietario = ?1 AND l.nombre LIKE :estado";
+    public List<Ticket> obtenerTicketsPorEstadoDePropietario(Usuario usuario, EstadoTicket estadoTicket){
+        String sql = "SELECT t FROM Ticket t WHERE t.usuarioidpropietario = ?1 AND t.estadoTicketcodigo = ?2";
         Query qry = this.em.createNamedQuery(sql);  
         qry.setParameter(1, usuario);
-        qry.setParameter("estado", "abierto");
+        qry.setParameter(2, estadoTicket);
         return qry.getResultList();
     }
+    
+    /**
+     * Función que permite recuperar los tickets por estado de un determinado usuario propietario (Ingresa String de estado)
+     */
+    public List<Ticket> obtenerTicketsPorEstadoDePropietario(Usuario usuario, String estadoTicket){
+        String sql = "SELECT t FROM Ticket t WHERE t.usuarioidpropietario = ?1 AND t.estadoTicketcodigo.nombre LIKE :estado";
+        Query qry = this.em.createNamedQuery(sql);  
+        qry.setParameter(1, usuario);
+        qry.setParameter("estado", estadoTicket);
+        return qry.getResultList();
+    }
+    
+    /**
+     * Función que permite recuperar los tickets por servicios de un determinado usuario propietario
+     */
+    public List<Ticket> obtenerTicketsPorServicioDePropietario(Usuario usuario, ServicioTicket servicioTicket){
+        String sql = "SELECT t FROM Ticket t WHERE t.usuarioidpropietario = ?1 AND t.servicioTicketcodigo = ?2";
+        Query qry = this.em.createNamedQuery(sql);  
+        qry.setParameter(1, usuario);
+        qry.setParameter(2, servicioTicket);
+        return qry.getResultList();
+    }
+    
+    /**
+     * Función que permite recuperar los tickets por servicios de un determinado usuario propietario (Ingresa string de servicio)
+     */
+    public List<Ticket> obtenerTicketsPorServicioDePropietario(Usuario usuario, String servicioTicket){
+        String sql = "SELECT t FROM Ticket t WHERE t.usuarioidpropietario = ?1 AND t.servicioTicketcodigo.nombre LIKE :servicio";
+        Query qry = this.em.createNamedQuery(sql);  
+        qry.setParameter(1, usuario);
+        qry.setParameter("servicio", servicioTicket);
+        return qry.getResultList();
+    }
+    
+    /**
+     * Función que permite recuperar los tickets por prioridad de un determinado usuario propietario
+     */
+    public List<Ticket> obtenerTicketsPorPrioridadDePropietario(Usuario usuario, PrioridadTicket prioridadTicket){
+        String sql = "SELECT t FROM Ticket t WHERE t.usuarioidpropietario = ?1 AND t.prioridadTicketcodigo = ?2";
+        Query qry = this.em.createNamedQuery(sql);  
+        qry.setParameter(1, usuario);
+        qry.setParameter(2, prioridadTicket);
+        return qry.getResultList();
+    }
+    
+    /**
+     * Función que permite recuperar los tickets por prioridad de un determinado usuario propietario
+     */
+    public List<Ticket> obtenerTicketsPorPrioridadDePropietario(Usuario usuario, String prioridadTicket){
+        String sql = "SELECT t FROM Ticket t WHERE t.usuarioidpropietario = ?1 AND t.prioridadTicketcodigo.nombre LIKE :prioridad";
+        Query qry = this.em.createNamedQuery(sql);  
+        qry.setParameter(1, usuario);
+        qry.setParameter("prioridad", prioridadTicket);
+        return qry.getResultList();
+    }
+    
+    /**
+     * Función que permite recuperar todos los tickets por estado
+     */
+    public List<Ticket> obtenerTodosLosTicketsPorEstado(String estado){
+        String sql = "SELECT t FROM Ticket t JOIN t.estadoTicketcodigo e WHERE e.nombre LIKE :estado";
+        Query qry = this.em.createNamedQuery(sql);  
+        qry.setParameter("estado", estado);
+        return qry.getResultList();
+    }
+    
+    
 }
