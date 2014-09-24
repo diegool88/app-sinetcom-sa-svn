3160d4707331397c2057895f3002ff524c5828f7
diegoaflores88 <diegoaflores88@gmail.com>
2014-09-24 21:57:18 +0000

    Servicios Nuevos para creacion de productos atomicos - Vistas y nuevos converters

---
+++ servicedesksinetcom/servicedesksinetcom-web/src/main/java/ec/com/sinetcom/validator/NumeroSerialValidator.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.sinetcom.validator;
+
+import ec.com.sinetcom.servicios.ProductoServicio;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author diegoflores
+ */
+@ManagedBean(name = "numeroSerialValidator")
+@RequestScoped
+public class NumeroSerialValidator implements Validator{
+    
+    @EJB
+    private ProductoServicio productoServicio;
+    
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(this.productoServicio.existeNumeroSerial((String)value)){
+            FacesMessage facesMessage = new FacesMessage("El número serial ingresado ya existe!");
+            facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(facesMessage);
+        }
+    }
+    
+}
