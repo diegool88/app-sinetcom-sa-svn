3dac71d8252993a99e754433740fa482e031472e
andreseagle007@gmail.com <andreseagle007@gmail.com@a04695b4-e77e-5f7e-00ee-a7b27f77682c>
2014-11-24 15:26:08 +0000

    Commit Servicios 24/11/2014 10:25AM

---
+++ trunk/servicedesksinetcom/servicedesksinetcom-ejb/src/main/java/ec/com/sinetcom/servicios/ClienteEmpresaServicio.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.com.sinetcom.servicios;
+
+import ec.com.sinetcom.dao.ClienteEmpresaFacade;
+import ec.com.sinetcom.dao.TipoEmpresaFacade;
+import ec.com.sinetcom.dao.UsuarioFacade;
+import ec.com.sinetcom.orm.ClienteEmpresa;
+import ec.com.sinetcom.orm.TipoEmpresa;
+import ec.com.sinetcom.orm.Usuario;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Andres
+ */
+@Stateless
+@LocalBean
+public class ClienteEmpresaServicio {
+    
+    @EJB
+    private ClienteEmpresaFacade clienteEmpresaFacade;
+    @EJB
+    private UsuarioFacade usuarioFacade;
+    @EJB
+    private TipoEmpresaFacade tipoEmpresaFacade;
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    public List<ClienteEmpresa> cargarClientesEmpresa(){
+        return clienteEmpresaFacade.findAll();
+    }
+    
+    public void crearClienteEmpresa(ClienteEmpresa empresa) {
+        clienteEmpresaFacade.create(empresa);
+    }
+    
+    public List<Usuario> cargarUsuarios() {
+        return usuarioFacade.findAll();
+    }
+    
+    public Usuario recuperarUsuario(Integer id) {
+        return usuarioFacade.find(id);
+    }
+    
+    public List<TipoEmpresa> cargarTiposEmpresa() {
+        return tipoEmpresaFacade.findAll();
+    }
+    
+    public TipoEmpresa recuperarTipoEmpresa(Integer id) {
+        return tipoEmpresaFacade.find(id);
+    }
+}
---
+++ trunk/servicedesksinetcom/servicedesksinetcom-ejb/src/main/java/ec/com/sinetcom/servicios/ContactoServicio.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.com.sinetcom.servicios;
+
+import ec.com.sinetcom.dao.ClienteEmpresaFacade;
+import ec.com.sinetcom.dao.ContactoFacade;
+import ec.com.sinetcom.orm.ClienteEmpresa;
+import ec.com.sinetcom.orm.Contacto;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Andres
+ */
+@Stateless
+@LocalBean
+public class ContactoServicio {    
+    
+    @EJB
+    private ContactoFacade contactoFacade;
+    @EJB
+    private ClienteEmpresaFacade clienteEmpresaFacade;
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    public List<Contacto> cragarContactos() {
+        return contactoFacade.findAll();
+    }
+    
+    public void crearContacto(Contacto contacto) {
+        contactoFacade.create(contacto);
+   }
+    
+    public List<ClienteEmpresa> cargarClientesEmpresas() {
+        return clienteEmpresaFacade.findAll();
+    }
+    
+    public ClienteEmpresa recuperarClienteEmpresa(String id) {
+        return clienteEmpresaFacade.find(id);        
+    }
+}
